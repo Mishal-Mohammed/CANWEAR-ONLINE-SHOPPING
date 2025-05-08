b61fedee87007acd9dc7f9aae0dfaa9d4324a32a
Mishal-Mohammed <116307951+Mishal-Mohammed@users.noreply.github.com>
2025-05-08 23:38:15 +0530

    Add files via upload

---
+++ test/layout/table.blade.php
@@ -0,0 +1,49 @@
+<div class="card">
+        <div class="card-header">
+        <h3 class="card-title">DataTable with default features</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+        <table id="example1" class="table table-bordered table-striped">
+            <thead>
+            <tr>
+            <th>Rendering engine</th>
+            <th>Browser</th>
+            <th>Platform(s)</th>
+            <th>Engine version</th>
+            <th>CSS grade</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+            <td>Misc</td>
+            <td>PSP browser</td>
+            <td>PSP</td>
+            <td>-</td>
+            <td>C</td>
+            </tr>
+            <tr>
+            <td>Other browsers</td>
+            <td>All others</td>
+            <td>-</td>
+            <td>-</td>
+            <td>U</td>
+            </tr>
+            </tbody>
+            <tfoot>
+            <tr>
+            <th>Rendering engine</th>
+            <th>Browser</th>
+            <th>Platform(s)</th>
+            <th>Engine version</th>
+            <th>CSS grade</th>
+            </tr>
+            </tfoot>
+        </table>
+        </div>
+        <!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+    </div>
+    <!-- /.col -->
+</div>
